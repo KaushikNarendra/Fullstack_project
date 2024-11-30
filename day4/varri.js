58db32427796285e71cc66754ce0372274e97890
KaushikNarendra <kaushiknarendra233@gmail.com>
2024-11-30 11:30:53 +0530

    Add files via upload

---
+++ day4/varri.js
@@ -0,0 +1,8 @@
+let num1 = 10;
+let num2 =20;
+let ans = num1 + num2;
+
+document.getElementById('output').innerHTML = `
+    <p>Number 1: ${num1}</p>
+    <p>Number 2: ${num2}</p>
+    <p>sum: ${ans}</p>`;
